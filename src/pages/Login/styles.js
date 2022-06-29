bc7642d34d4e047d222c5656cf070de7a0e33be8
Carlos <caduzin11324@gmail.com>
2022-06-29 01:00:51 -0300

    finishing login page

---
+++ src/pages/Login/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const LoginStyle = styled.div`
+    display: flex;
+    position: relative;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #246eb1;
+`;
+
+export default LoginStyle;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
 import PrivateRoute from "./utils/PrivateRoute";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={"/Login"} component={() => {return(<div>Página 1</div>)}} />
+        <Route exact path={"/login"} component={() => <Login />} />
         <PrivateRoute exact path={"/page2"} component={() => {return(<div>Página 2</div>)}} />
         <PrivateRoute exact path={"/page3"} component={() => {return(<div>Página 3</div>)}} />
       </Switch>
