a03d9400cd0d4dd9733b6e3697b95e2b29dd8114
Carlos <caduzin11324@gmail.com>
2022-06-28 21:23:26 -0300

    fixing the database

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./utils/PrivateRoute";
-import Pag1 from "./pages/pag1";
-import Pag2 from "./pages/pag2";
-import Pag3 from "./pages/pag3";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={"/page1"} component={Pag1} />
-        <PrivateRoute exact path={"/page2"} component={Pag2} />
-        <PrivateRoute exact path={"/page3"} component={Pag3} />
+        <Route exact path={"/Login"} component={() => {return(<div>Página 1</div>)}} />
+        <PrivateRoute exact path={"/page2"} component={() => {return(<div>Página 2</div>)}} />
+        <PrivateRoute exact path={"/page3"} component={() => {return(<div>Página 3</div>)}} />
       </Switch>
     </BrowserRouter>
   );
