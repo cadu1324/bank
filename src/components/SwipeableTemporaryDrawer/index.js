9c7ed936a93efda3465d65330f18b409bd5d6151
Carlos <caduzin11324@gmail.com>
2022-07-01 17:42:34 -0300

    finishing the home

--- src/components/SwipeableTemporaryDrawer/index.js
+++ src/components/SwipeableTemporaryDrawer/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import AppBar from "@mui/material/AppBar";
@@ -6,20 +6,48 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import PersonIcon from "@mui/icons-material/Person";
 import Card from "../Card";
 import { Container } from "@mui/material";
-import DropDown from "../DropDown"
+import DropDown from "../DropDown";
+import axios from "axios";
 
 const drawerWidth = 240;
 
 const ClippedDrawer = () => {
+  const [event, setEvent] = useState(2);
+  const [name, setName] = useState("");
+  const [bank, setBank] = useState("");
+  const [agency, setAgency] = useState("");
+  const [account, setAccount] = useState("");
+  const [document, setDocument] = useState("");
+  const [show, setShow] = useState(false);
 
+  const getData = async () => {
+    const response = await axios.get(
+      `http://localhost:5000/data?id=${event.target.value}`
+    );
+    response.data.map((option) => {
+      setName(option.name);
+      setBank(option.bank.bankName);
+      setAgency(option.bank.agency);
+      setAccount(option.bank.account);
+      setDocument(option.document);
+    });
+    setShow(true);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [event]);
+
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -47,41 +75,39 @@ const ClippedDrawer = () => {
         <Toolbar />
         <Box sx={{ overflow: "auto" }}>
           <List>
-            {["Informations"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
+            <ListItemButton>
+              <ListItemIcon sx={{ gap: 4 }}>
+                <PersonIcon />
+                {"Informações"}
+              </ListItemIcon>
+              <ListItemText />
+            </ListItemButton>
           </List>
         </Box>
       </Drawer>
-      <Container component="main" sx={{ marginTop: 20, display: "flex", justifyContent: "center", alignContent: "center", gap: 20 }}>
-        <DropDown />
-        <Box 
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 5
-          }}
-        >
-          <Card text={"alo"}/>
-          <Card text={"20202002"}></Card>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 5
-          }}
-        >
-          <Card text={"alo"} />
-          <Card text={"alo"}></Card>
-        </Box>
+      <Container
+        component="main"
+        sx={{
+          marginTop: 20,
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          gap: 20,
+        }}
+      >
+        <DropDown setValue={setEvent} value={event} />
+        {show ? (
+          <Card
+            name={name}
+            bank={bank}
+            agency={`Agência: ${agency}`}
+            account={`Conta: ${account}`}
+            document={`Documento: ${document}`}
+            data={`Data: ${date}`}
+          />
+        ) : (
+          <Card />
+        )}
       </Container>
     </Box>
   );
